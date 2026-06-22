25141123b85120f346f03ae4da26295f5af24eb9
Aditya Thombre <adityathombre1440@gmail.com>
2026-06-22 18:08:50 +0530

    week 3 assignments

---
+++ 03_middleware/assignments/01-middlewares/02-ratelimitter.js
@@ -0,0 +1,51 @@
+const request = require('supertest');
+const assert = require('assert');
+const express = require('express');
+const app = express();
+// You have been given an express server which has a few endpoints.
+// Your task is to create a global middleware (app.use) which will
+// rate limit the requests from a user to only 5 request per second
+// If a user sends more than 5 requests in a single second, the server
+// should block them with a 404.
+// User will be sending in their user id in the header as 'user-id'
+// You have been given a numberOfRequestsForUser object to start off with which
+// clears every one second
+
+function rateLimiter(req,res,next){
+  const userID = req.headers.user_id;
+
+  if(!userID ){
+    res.status(400).json({"msg": "missing user ID"})
+  }
+  if(!numberOfRequestsForUser[userID]){
+    numberOfRequestsForUser[userID] = 0;
+  }
+
+  if(numberOfRequestsForUser[userID] > 5){
+    console.error(`${userID} rate limit exceeded, wait some time`);
+    res.status(404).json({
+      "msg":"Rate Limit Exceeded"
+    })
+    return;
+  }
+  numberOfRequestsForUser[userID]++;
+  next();
+}
+
+let numberOfRequestsForUser = {};
+setInterval(() => {
+    numberOfRequestsForUser = {};
+}, 1000)
+
+app.use(rateLimiter)
+
+app.get('/user', function(req, res) {
+  res.status(200).json({ name: 'john' });
+});
+
+app.post('/user', function(req, res) {
+  res.status(200).json({ msg: 'created dummy user' });
+});
+
+
+module.exports = app;
